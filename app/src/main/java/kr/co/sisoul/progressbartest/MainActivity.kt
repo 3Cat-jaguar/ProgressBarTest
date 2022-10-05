de6bf1ca5bf73daeda4701ddf10515059d6d8d1c
YuriLee <yuri.lee@sisoul.co.kr>
2022-10-05 16:04:58 +0900

    set custom ring type progress bar

--- app/src/main/java/kr/co/sisoul/progressbartest/MainActivity.kt
+++ app/src/main/java/kr/co/sisoul/progressbartest/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.sisoul.progressbartest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import kr.co.sisoul.progressbartest.customview.CustomRingProgressBar
+import kr.co.sisoul.progressbartest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.setRatioBtn.setOnClickListener {
+            val ratio = binding.editRatio.text.toString().toInt()?:0
+            binding.ringProgress.setRatio(ratio)
+        }
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/kr/co/sisoul/progressbartest/customview/CustomRingProgressBar.kt
@@ -0,0 +1,33 @@
+package kr.co.sisoul.progressbartest.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import kr.co.sisoul.progressbartest.R
+import kr.co.sisoul.progressbartest.databinding.CustomProgressbarRingBinding
+
+class CustomRingProgressBar @JvmOverloads
+constructor(context : Context, attrs: AttributeSet?=null, defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val binding by lazy { CustomProgressbarRingBinding.inflate(LayoutInflater.from(context), this, false ) }
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomRingProgressBar,
+            0, 0).apply {
+            try {
+                val ratio = getInt(R.styleable.CustomRingProgressBar_customProgressRatio, 5)
+                setRatio(ratio)
+            } finally {
+                recycle()
+                addView(binding.root)
+            }
+        }
+    }
+
+    fun setRatio(ratio : Int) {
+        binding.ringProgressBar.progress = ratio
+        binding.ringProgressBarText.text = "${ratio}%"
+    }
+}
